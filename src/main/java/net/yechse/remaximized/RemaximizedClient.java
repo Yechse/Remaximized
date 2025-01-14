52387fd081dbbef684643bfec5392df8f5d26371
Yechse <umutegeileri15@gmail.com>
2025-01-15 01:28:04 +0300

    first commit

---
+++ src/main/java/net/yechse/remaximized/RemaximizedClient.java
@@ -0,0 +1,10 @@
+package net.yechse.remaximized;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RemaximizedClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
