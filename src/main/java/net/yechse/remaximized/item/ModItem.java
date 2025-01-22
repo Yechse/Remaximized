11a5b695c1dd43420860bc256a61eee190b2fcd1
Yechse <umutegeileri15@gmail.com>
2025-01-22 23:05:56 +0300

    moditem class & items

---
+++ src/main/java/net/yechse/remaximized/item/ModItem.java
@@ -0,0 +1,26 @@
+package net.yechse.remaximized.item;
+
+import net.minecraft.registry.*;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.util.Identifier;
+import net.yechse.remaximized.Remaximized;
+
+public class ModItem {
+    public static Item register(Item item, String id) {
+        Identifier itemID = Identifier.of(Remaximized.MOD_ID, id);
+        Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
+        return registeredItem;
+    }
+
+    public static final Item TRASSIUM_SHARD = register(new Item(new Item.Settings()), "trassium_shard");
+    public static final Item TRASSIUM_DUST = register(new Item(new Item.Settings()), "trassium_dust");
+
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+        .register((itemGroup) -> itemGroup.add(ModItem.TRASSIUM_SHARD));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+        .register((itemGroup) -> itemGroup.add(ModItem.TRASSIUM_DUST));
+    }
+}
