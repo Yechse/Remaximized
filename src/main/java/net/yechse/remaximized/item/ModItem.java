de30deb2e1dccda5b9487c9f72972706e0aaecc8
Yechse <umutegeileri15@gmail.com>
2025-01-22 23:11:14 +0300

    another item

--- src/main/java/net/yechse/remaximized/item/ModItem.java
+++ src/main/java/net/yechse/remaximized/item/ModItem.java
@@ -14,13 +14,16 @@ public class ModItem {
         return registeredItem;
     }
 
+    public static final Item TRASSIUM_DUST = register(new Item(new Item.Settings()), "trassium_gem");
     public static final Item TRASSIUM_SHARD = register(new Item(new Item.Settings()), "trassium_shard");
-    public static final Item TRASSIUM_DUST = register(new Item(new Item.Settings()), "trassium_dust");
+    public static final Item TRASSIUM_GEM = register(new Item(new Item.Settings()), "trassium_dust");
 
     public static void initialize() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
         .register((itemGroup) -> itemGroup.add(ModItem.TRASSIUM_SHARD));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
         .register((itemGroup) -> itemGroup.add(ModItem.TRASSIUM_DUST));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+        .register((itemGroup) -> itemGroup.add(ModItem.TRASSIUM_GEM));
     }
 }
